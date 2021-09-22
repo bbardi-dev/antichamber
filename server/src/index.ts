9b123738f27b0259aeb1ccca9e63a8a98c5ffd88
bbardi-dev <bbardi.dev@gmail.com>
2021-09-22 16:59:34 +0200

    working

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,11 +3,14 @@ import express from "express";
 import config from "./config/config";
 import log from "./config/logger";
 import routes from "./routes";
+//@ts-ignore
+import cors from "cors";
 
 const main = async () => {
   const app = express();
 
   app.use(express.json());
+  app.use(cors());
   app.use(express.urlencoded({ extended: false }));
 
   app.listen(config.server.port, config.server.hostname, () => {

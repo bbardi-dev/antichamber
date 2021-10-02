2d96189c6a15c0447f8df50adde649d4aaa992c9
bbardi-dev <bbardi.dev@gmail.com>
2021-10-02 20:42:31 +0200

    deploying frontend

---
+++ client/components/SearchBar.tsx
@@ -0,0 +1,50 @@
+import { NextPage } from "next";
+import React, { Dispatch, SetStateAction, useEffect } from "react";
+import { mainURL } from "../constants";
+
+interface Article {
+  createdAt: string;
+  title: string;
+  source: string;
+  link: string;
+}
+
+interface Props {
+  setCurrentArticles: Dispatch<SetStateAction<Article[]>>;
+  setDate: Dispatch<SetStateAction<Date | null>>;
+  searchQuery: string;
+  setSearchQuery: Dispatch<SetStateAction<string>>;
+}
+
+const SearchBar: NextPage<Props> = ({
+  setCurrentArticles,
+  setDate,
+  searchQuery,
+  setSearchQuery,
+}) => {
+  useEffect(() => {
+    console.log(searchQuery);
+    if (searchQuery.length > 2) {
+      fetch(`${mainURL}/articles?title=${searchQuery}`)
+        .then((res) => res.json())
+        .then((data) => setCurrentArticles(data))
+        .catch((err) => console.log(err));
+      setDate(null);
+    }
+  }, [searchQuery, setDate, setCurrentArticles]);
+
+  return (
+    <div className='search'>
+      <input
+        value={searchQuery}
+        onChange={(e) => setSearchQuery(e.target.value)}
+        type='text'
+        id='article-search'
+        placeholder='Keress a cikkek között'
+        name='title'
+      />
+    </div>
+  );
+};
+
+export default SearchBar;
